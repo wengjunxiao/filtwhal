f318ed1b524ec6094ec269051cf8d307ad3abb12
wengjunxiao <lwengjunxiao@hotmail.com>
2017-05-23 22:39:13 +0800

    table

---
+++ resources/views/web/header.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html id="ng-app" data-framework="angularjs" ng>
+<head>
+    <meta charset="UTF-8">
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/flags.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/flags.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/admin.css') }}" rel="stylesheet">
+    <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="https://rawgit.com/raoulus/angular-simple-pagination/github/dist/angular-simple-pagination.css"></link>
+    <!-- Scripts -->
+	<script data-main="/js/AMD" src="//cdnjs.cloudflare.com/ajax/libs/require.js/2.1.15/require.min.js"></script>
+	<base href="/">
+<title>生鲜网</title>
+</head>
+<body>
+<br />
