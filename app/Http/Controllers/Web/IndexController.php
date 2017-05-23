b4d04c8f0877d72bdad499c295f8e02a5ecc1d36
wengjunxiao <lwengjunxiao@hotmail.com>
2017-05-23 22:22:12 +0800

    table

--- app/Http/Controllers/Web/IndexController.php
+++ app/Http/Controllers/Web/IndexController.php
@@ -2,17 +2,6 @@
 
 namespace App\Http\Controllers\Web;
 
-use App\Product;
-use App\ProductPacking;
-use App\ProductPackingSpec;
-use App\Transaction;
-
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
-use \Cart;
-use Request;
-use Illuminate\Support\Facades\Auth as Auth;
-
 class IndexController extends Controller
 {
     /**
