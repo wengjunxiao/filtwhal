980e6b37cb61ba8878fc6cd55650c2a8c0aafbeb
wengjunxiao <lwengjunxiao@hotmail.com>
2017-05-28 21:05:02 +0800

    table

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Auth\Registrar;
-use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
 class AuthController extends Controller {
 
@@ -18,7 +17,6 @@ class AuthController extends Controller {
 	|
 	*/
 
-	use AuthenticatesAndRegistersUsers;
 
 	/**
 	 * Create a new authentication controller instance.
