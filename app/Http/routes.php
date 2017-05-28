c105a8eb25314e9ff888b0da17e72ffa71ed7a63
wengjunxiao <lwengjunxiao@hotmail.com>
2017-05-28 21:02:01 +0800

    table

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,12 +12,15 @@
 */
 
 /** 认证 */
+ 
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+ 
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
-Route::controllers([
-    'auth' => 'Auth\AuthController',
-    'password' => 'Auth\PasswordController',
-]);
-
 /** Web(网站部分) */
 
 /** 网站页面部分 首页 详细页 搜索页 */
