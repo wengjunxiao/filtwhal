a850e5dfa6188f8bdbe02c2315cbae2298d181ef
wengjunxiao <lwengjunxiao@hotmail.com>
2017-05-28 21:10:17 +0800

    table

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Auth\Registrar;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
 class AuthController extends Controller {
 
@@ -16,7 +17,6 @@ class AuthController extends Controller {
 	| a simple trait to add these behaviors. Why don't you explore it?
 	|
 	*/
-
 
 	/**
 	 * Create a new authentication controller instance.
