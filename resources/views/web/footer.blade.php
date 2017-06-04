4c290c15118be21a48605d04dc83e1701d20cfe2
wengjunxiao <lwengjunxiao@hotmail.com>
2017-06-04 19:01:05 +0800

    page

--- resources/views/web/footer.blade.php
+++ resources/views/web/footer.blade.php
@@ -1,8 +1,8 @@
 <br />
 </body>
-	 <ul>
+	 <ul style="list-style:none;">
         <li><a href="{{ url('/about') }}" TARGET="_self">About</a></li>
-        <li><a href="{{ url('/contact') }}" TARGET="_self">Contact Us</a></li>
+        <li><a href="{{ url('/contact_us') }}" TARGET="_self">Contact Us</a></li>
         <li><a href="{{ url('/recruit') }}" TARGET="_self">Recruit</a></li>
     </ul>
 </html>
