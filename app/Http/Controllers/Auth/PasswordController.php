f48e3935cec45376b42b095532b165567bc86593
wengjunxiao <lwengjunxiao@hotmail.com>
2017-06-10 21:19:56 +0800

    m

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -56,7 +56,7 @@ class PasswordController extends Controller {
 				return redirect()->back()->with('status', trans($response));
 
 			case PasswordBroker::INVALID_USER:
-				return redirect()->back()->withErrors(['email' => trans($response)]);
+				return redirect($this->getEmail())->withErrors(['email' => trans($response)]);
 		}
 	}
 
