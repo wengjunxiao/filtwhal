1b6d8cb7d1d5ddf47c5180b59ecc828f97c268ad
wengjunxiao <lwengjunxiao@hotmail.com>
2017-07-09 21:47:07 +0800

    aa

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -15,7 +15,7 @@
    <br>
 
   <div class="">
-      <img class="home-main" ng-src="/img/home_main.jpg">
+      <img class="home-main" src="/img/home_main.jpg">
       <button>Submit Your Needs</button>
   </div>
    <ul class ="nolist-ul">
