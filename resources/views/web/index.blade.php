79c754103355bfb05798afa16e884b350d4aee18
wengjunxiao <lwengjunxiao@hotmail.com>
2017-07-10 21:34:06 +0800

    aa

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -17,8 +17,8 @@
   <div style="text-align: center;">
       <img style="width: 70%" class="home-main" src="/img/home_main.png">
   </div>
-  <div>
-      <button type="button" class="btn btn-danger">Submit Your Needs</button>
+  <div style="text-align: center;">
+      <button type="button" class="btn btn-outline-danger btn-lg btn-block">Submit Your Needs</button>
   </div>
    <ul class ="nolist-ul">
         <li ><a style="color: black" href="{{ url('/about') }}" TARGET="_self">About</a></li>
