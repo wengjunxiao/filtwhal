145c9e2405a807b3e1eead87b1e135889ed12b3b
wengjunxiao <lwengjunxiao@hotmail.com>
2017-08-24 19:56:38 +0800

    a

--- resources/views/web/submit_need.blade.php
+++ resources/views/web/submit_need.blade.php
@@ -1,6 +1,6 @@
 @extends('web.app')
 @section('content')
-<link href="{{ asset('/css/index.css') }}" rel="stylesheet">
+<link href="{{ asset('/css/submit-need.css') }}" rel="stylesheet">
 <!-- <div style="height: 600px" ng-controller="ViewHomeController">
     <div uib-carousel active="active" interval="myInterval">
       <div uib-slide ng-repeat="slide in slides track by slide.id" index="slide.id" style="height:550px;">
@@ -17,12 +17,32 @@
    <div >
        <form class="form" role="form" method="POST" action="{{ url('/submit-need/submit') }}">
           <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <div>お名前<input name="name" ></div>           
-                <div>メールアドレス<input name="mail"></div>
-                <div>所属/会社<input name="company"></div>
-                <div>お問い合せの種類<input name="question_div" ></div>
-                <div>お問い合せ内容<input name="question" ></div>
-            <button type="submit" name="submit" value="buy">Submit</button>
+              <div>
+                  <dl>
+                      <dt>お名前</dt>
+                      <dd><input type="text" name="name"></dd>
+                  </dl>
+                  <dl>
+                      <dt>メールアドレス</dt>
+                      <dd><input type="email" name="mail"></dd>
+                  </dl>
+                  <dl>
+                      <dt>所属/会社</dt>
+                      <dd><input type="text" name="company"></dd>
+                  </dl>
+                  <dl>
+                      <dt>お問い合せの種類</dt>
+                      <dd><input type="text" name="question_div"></dd>
+                  </dl>
+                  <dl>
+                      <dt>お問い合せ内容</dt>
+                      <dd><input type="text" name="question"></dd>
+                  </dl>
+                  <dl>
+                      <dd><button type="submit" name="submit" value="buy">Submit</button></dd>
+                  </dl>
+              </div>
+            
       </form>
   </div>
 
