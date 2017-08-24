799c9dd2b471c9b10b8c856604786c23103558d0
wengjunxiao <lwengjunxiao@hotmail.com>
2017-08-24 22:34:50 +0800

    a

--- resources/views/web/our_products.blade.php
+++ resources/views/web/our_products.blade.php
@@ -3,7 +3,7 @@
 <link href="{{asset('/css/index.css') }}" rel="stylesheet">
 <div style="height: 600px" ng-controller="ViewHomeController">
     <div uib-carousel active="active" interval="myInterval">
-      <div uib-slide ng-repeat="slide in slides track by slide.id" index="slide.id" style="height:550px;">
+      <div uib-slide ng-repeat="slide in slides track by slide.id" index="slide.id" style="height:800px;">
         <div class="block"> 
         <img style="width: 2500px;margin-left: -250px;margin-top: -350px"class="inner" ng-src="@{{slide.image}}">
         </div>
