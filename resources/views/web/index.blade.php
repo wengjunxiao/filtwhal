4ab82b39e2cb0cc3b70b82ba08e971e3e4025ef8
wengjunxiao <lwengjunxiao@hotmail.com>
2017-10-23 22:22:28 +0800

    f

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -29,13 +29,13 @@
 <!--   <div style="margin-left: 200px">
       <img style="width: 700px" class="home-main" src="/img/home_main.png">
   </div> -->
-  <div style="padding-top:100px; padding-bottom:100px;padding-left: 500px;background-color: #D8D8D8；text-align: center;">
+  <div style="padding-top:100px; padding-bottom:100px;background-color: #D8D8D8；padding-left: 10%;width: 80%>
       <button type="button" class="btn btn-submit" ng-click="goToSubmitNeed()">Submit Your Needs</button>
       <button type="button" class="btn btn-submit" style="margin-left: 100px; color: #BC2525; background-color: white;" ng-click="">Designer Register</button>
   </div>
 
  
-  <div style="clear: both; margin-left: 20%;width: 80%">
+  <div style="clear: both; margin-left: 10%;width: 80%">
       <div style="float: left;">
         <img style="width: 300px;margin-left: 50px;" class="inner" ng-src="/img/tky.png">
       </div>    
