e7e80b69e7f86d09255c83c71ee63055deec2935
wengjunxiao <lwengjunxiao@hotmail.com>
2017-11-15 19:55:40 +0800

    table

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -91,30 +91,30 @@
   </div>
   <div style="clear:both; margin-top: 60px">
      <p style="margin-bottom:20px; text-align:center;font-weight: 700; font-size: -webkit-xxx-large; ">Contact Us</p>
-     <form style=" margin-left:40%; margin-top: 60px" class="form" role="form" method="POST" action="{{ url('/contact-us') }}">
+     <form style=" margin-left:30%; margin-top: 60px" class="form" role="form" method="POST" action="{{ url('/contact-us') }}">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
             <div class="submit-table">
                 <div>
                 <dl style="float:left; margin-right: 100px">
                     <dt>お名前</dt>
-                    <dd><input type="text" class="submit-text" name="name"></dd>
+                    <dd><input type="text" class="submit-text" style="width: 300px;" name="name"></dd>
                 </dl>
                 <dl>
                     <dt>メールアドレス</dt>
-                    <dd><input type="email" class="submit-text" name="mail"></dd>
+                    <dd><input type="email" class="submit-text" style="width: 300px;" name="mail"></dd>
                 </dl>
                 </div>
                 <dl>
                     <dt>所属/会社</dt>
-                    <dd><input type="text" class="submit-text" name="company" style="width: 425px;"></dd>
+                    <dd><input type="text" class="submit-text" name="company" style="width: 500px;"></dd>
                 </dl>
                 <dl>
                     <dt>お問い合せの種類</dt>
-                    <dd><input type="text" class="submit-text" name="question_div" style="width: 425px;"></dd>
+                    <dd><input type="text" class="submit-text" name="question_div" style="width: 500px;"></dd>
                 </dl>
                 <dl>
                     <dt style="vertical-align: top;">お問い合せ内容</dt>
-                    <dd><textarea type="text" style="height:150px;width: 425px" name="question"></textarea></dd>
+                    <dd><textarea type="text" class="submit-text" style="height:150px;width: 500px" name="question"></textarea></dd>
                 </dl>
                 <dl>
                     <dd><button type="submit" name="submit" value="buy">Submit</button></dd>
