765e34d30c31a5b6333f16172b5e03ad2c47b973
wengjunxiao <lwengjunxiao@hotmail.com>
2017-11-15 22:13:44 +0800

    table

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -35,7 +35,7 @@ class RedirectIfAuthenticated {
 	{
 		if ($this->auth->check())
 		{
-			return new RedirectResponse(url('/register-ok'));
+			return new RedirectResponse(url('/home'));
 		}
 
 		return $next($request);
