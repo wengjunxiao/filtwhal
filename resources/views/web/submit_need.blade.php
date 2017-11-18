11e373dfd88a19ed7d225ab93c7f6ca6d8146c49
wengjunxiao <lwengjunxiao@hotmail.com>
2017-11-18 18:26:36 +0800

    table

--- resources/views/web/submit_need.blade.php
+++ resources/views/web/submit_need.blade.php
@@ -16,33 +16,34 @@
 
    <div >
        <form  class="form" role="form" method="POST" action="{{ url('/submit-need/submit') }}">
-          <input type="hidden" name="_token" value="{{ csrf_token() }}">
-              <div class="submit-table">
-                  <dl>
-                      <dt>お名前</dt>
-                      <dd><input type="text" name="name"></dd>
-                  </dl>
-                  <dl>
-                      <dt>メールアドレス</dt>
-                      <dd><input type="email" name="mail"></dd>
-                  </dl>
-                  <dl>
-                      <dt>所属/会社</dt>
-                      <dd><input type="text" name="company"></dd>
-                  </dl>
-                  <dl>
-                      <dt>お問い合せの種類</dt>
-                      <dd><input type="text" name="question_div"></dd>
-                  </dl>
-                  <dl>
-                      <dt style="vertical-align: top;">お問い合せ内容</dt>
-                      <dd><textarea type="text" style="height:150px;width: 250px" name="question"></textarea></dd>
-                  </dl>
-                  <dl>
-                      <dd><button type="submit" name="submit" value="buy">Submit</button></dd>
-                  </dl>
-              </div>
-            
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <div class="submit-table">
+                <div>
+                <dl style="float:left; margin-right: 50px">
+                    <dt><p class="form-box-text">お名前</p></dt>
+                    <dd><input type="text" class="submit-text" style="width: 150px;" name="name"></dd>
+                </dl>
+                <dl>
+                    <dt><p class="form-box-text">メールアドレス</p></dt>
+                    <dd><input type="email" class="submit-text" style="width: 300px;" name="mail"></dd>
+                </dl>
+                </div>
+                <dl>
+                    <dt><p class="form-box-text">所属/会社</p></dt>
+                    <dd><input type="text" class="submit-text" name="company" style="width: 500px;"></dd>
+                </dl>
+                <dl>
+                    <dt><p class="form-box-text">お問い合せの種類</p></dt>
+                    <dd><input type="text" class="submit-text" name="question_div" style="width: 500px;"></dd>
+                </dl>
+                <dl>
+                    <dt style="vertical-align: top;"><p class="form-box-text">お問い合せ内容</p></dt>
+                    <dd><textarea type="text" class="submit-text" style="height:150px;width: 500px" name="question"></textarea></dd>
+                </dl>
+                <dl>
+                    <dd><button type="submit" class="btn btn-contact" name="submit" style="margin-left: 150px;"  value="submit">提出</button></dd>
+                </dl>
+            </div>
       </form>
   </div>
 
