b6672eb09e9b84b9f237465d0fc77d7fb5234220
wengjunxiao <lwengjunxiao@hotmail.com>
2017-11-19 16:05:40 +0800

    f

--- public/js/app.js
+++ public/js/app.js
@@ -89,17 +89,19 @@ define(['angular'], function (angular) {
          $scope.goToRegister = function(){
             document.location.href = "/designer-register";
         };
+
+         $scope.gotoElement = function (eID){
+	      // set the location.hash to the id of
+	      // the element you wish to scroll to.
+	      $location.hash('bottom');
+	 
+	      // call $anchorScroll()
+	      anchorSmoothScroll.scrollTo(eID);
+	      
+	    };
     });
     
-    $scope.gotoElement = function (eID){
-      // set the location.hash to the id of
-      // the element you wish to scroll to.
-      $location.hash('bottom');
- 
-      // call $anchorScroll()
-      anchorSmoothScroll.scrollTo(eID);
-      
-    };
+   
     
 	return app;
 
