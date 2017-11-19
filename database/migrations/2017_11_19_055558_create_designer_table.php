edaf3102cf16c89e6c4e78772cd227b110e4a150
wengjunxiao <lwengjunxiao@hotmail.com>
2017-11-19 14:00:22 +0800

    table

--- database/migrations/2017_11_19_055558_create_designer_table.php
+++ database/migrations/2017_11_19_055558_create_designer_table.php
@@ -12,7 +12,16 @@ class CreateDesignerTable extends Migration {
 	 */
 	public function up()
 	{
-		//
+		Schema::create('designers', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string("name");
+	        $table->string("mail");
+	        $table->string("company");
+	        $table->string("skill");
+			$table->timestamps();
+		});
+		
 	}
 
 	/**
