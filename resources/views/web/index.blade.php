d7b9d3abbc831f35fea23cfb3f06cdfa8a488e41
wengjunxiao <lwengjunxiao@hotmail.com>
2017-11-19 14:46:43 +0800

    table

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -123,12 +123,13 @@
                 </dl>
             </div>
       </form>
-        <p>
--      ドメイン:www.filtwhal.co.jp<br>
--      住所：東京都港区赤坂7-3-38  カナダ大使館　1F<br>
--      電話：03-6894-7676<br>
--      Email:info@filtwhal.cojp<br>
--     </p>
+        <p style="text-align: center;margin-top: 20px;font-family: Meiryo UI;">
+        --------------------------------------------<br>
+        ドメイン:www.filtwhal.co.jp<br>
+        住所：東京都港区赤坂7-3-38  カナダ大使館　1F<br>
+        電話：03-6894-7676<br>
+        Email:info@filtwhal.cojp<br>
+        </p>
   </div>
 <!--   <div style="clear: both;">
     <ul class ="nolist-ul">
